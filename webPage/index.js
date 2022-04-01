26c047b5bee2658db00ac0923c0402fb05f4af50
unknown <vishalbadukale00@gmail.com>
2022-04-01 20:14:09 +0530

    page

---
+++ webPage/index.js
@@ -0,0 +1,7 @@
+function toggle()
+    {
+        var blur =document.getElementById('all')
+        blur.classList.toggle('active')
+        var pop =document.getElementById('signUpForm')
+        pop.classList.toggle('active')
+    }
