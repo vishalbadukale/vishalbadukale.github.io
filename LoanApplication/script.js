aff3aa9b5c209dab30e45ffc2c8177e7cc567b42
Vishal <vishalbadukale01@gmail.com>
2022-07-12 15:58:12 +0530

    The amount pattern change and opt length

--- LoanApplication/script.js
+++ LoanApplication/script.js
@@ -122,37 +122,37 @@ document.getElementById('pan').addEventListener('blur', panNumber);
 
 const a = [
 	'',
-	'one ',
-	'two ',
-	'three ',
-	'four ',
-	'five ',
-	'six ',
-	'seven ',
-	'eight ',
-	'nine ',
-	'ten ',
-	'eleven ',
-	'twelve ',
-	'thirteen ',
-	'fourteen ',
-	'fifteen ',
-	'sixteen ',
-	'seventeen ',
-	'eighteen ',
-	'nineteen ',
+	'One ',
+	'Two ',
+	'Three ',
+	'Four ',
+	'Five ',
+	'Six ',
+	'Seven ',
+	'Eight ',
+	'Nine ',
+	'Ten ',
+	'Eleven ',
+	'Twelve ',
+	'Thirteen ',
+	'Fourteen ',
+	'Fifteen ',
+	'Sixteen ',
+	'Seventeen ',
+	'Eighteen ',
+	'Nineteen ',
 ];
 const b = [
 	'',
 	'',
-	'twenty',
-	'thirty',
-	'forty',
-	'fifty',
-	'sixty',
-	'seventy',
-	'eighty',
-	'ninety',
+	'Twenty',
+	'Thirty',
+	'Forty',
+	'Fifty',
+	'Sixty',
+	'Seventy',
+	'Eighty',
+	'Ninety',
 ];
 
 const onlyNumber = (n) => {
@@ -176,27 +176,27 @@ function inWords(num) {
 	var str = '';
 	str +=
 		n[1] != 0
-			? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore '
+			? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'Crore '
 			: '';
 	str +=
 		n[2] != 0
-			? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh '
+			? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'Lakh '
 			: '';
 	str +=
 		n[3] != 0
-			? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand '
+			? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'Thousand '
 			: '';
 	str +=
 		n[4] != 0
 			? 
 			  (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) +
-			  'hundred '
+			  'Hundred '
 			: '';
 	str +=
 		n[5] != 0
 			? (str != '' ? 'and ' : '') +
 			  (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) +
-			  'RS '
+			  'RS'
 			: '';
 	return str;
 }
